34644b3fc4de2a9109029dbf2c2bfa97a776280b
shofol <iash@liveimplevista.onmicrosoft.com>
2024-04-03 01:11:55 +0600

    Firsst commit

---
+++ demo.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    printf("hello World hello!");
+    return 0;
+}
+
---
+++ first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+  int number1;
+  int number2;
+  char p;
+  scanf("%d%% %d%%",&number1,&number2);
+  printf("%d%% %d%%",number1,number2);
+    return 0;
+}
+
